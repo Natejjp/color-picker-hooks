cd4a2c18ac318ce7c117e01c843ba522ba29cf14
Nathan Phame <diyf2021@gmail.com>
2021-06-01 17:02:15 -0400

    Added hue slider input by using helper function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react'
 
 export function App() {
-  const [color, setColor] = useState({
-    hue: 0,
-    saturation: 0,
-    lighting: 0,
-  })
+  const [hue, setHue] = useState(0)
+
+  function handleHueChange(event) {
+    setHue(event.target.value)
+  }
+
   return (
     <>
       <h1>Color Picker</h1>
-      <div className="box"></div>
-      <p>H: </p>
+      <div
+        className="box"
+        style={{ backgroundColor: `hsl(${hue}, 100%, 50%)` }}
+      >
+        hello
+      </div>
+      <p>H: {hue}</p>
+      <input type="range" value={hue} max="360" onInput={handleHueChange} />
       <p>S: </p>
       <p>L: </p>
     </>
