11c9195fe2002b9e4681df36fed667e83fd7a443
Nathan Phame <diyf2021@gmail.com>
2021-06-01 14:24:52 -0400

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [color, setColor] = useState({
+    hue: 0,
+    saturation: 0,
+    lighting: 0,
+  })
+  return (
+    <>
+      <h1>Color Picker</h1>
+      <div className="box"></div>
+      <p>H: </p>
+      <p>S: </p>
+      <p>L: </p>
+    </>
+  )
 }
