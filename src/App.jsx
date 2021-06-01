a6c412c354160caad621905eeeed7f6e4d4fdc8b
Nathan Phame <diyf2021@gmail.com>
2021-06-01 17:13:31 -0400

    Added saturation and lighting inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 
 export function App() {
   const [hue, setHue] = useState(0)
+  const [saturation, setSaturation] = useState(0)
+  const [lighting, setLighting] = useState(0)
 
   function handleHueChange(event) {
     setHue(event.target.value)
+  }
+  function handleSaturationChange(event) {
+    setSaturation(event.target.value)
+  }
+
+  function handleLightingChange(event) {
+    setLighting(event.target.value)
   }
 
   return (
@@ -12,14 +21,26 @@ export function App() {
       <h1>Color Picker</h1>
       <div
         className="box"
-        style={{ backgroundColor: `hsl(${hue}, 100%, 50%)` }}
+        style={{ backgroundColor: `hsl(${hue}, ${saturation}%, ${lighting}%)` }}
       >
         hello
       </div>
       <p>H: {hue}</p>
       <input type="range" value={hue} max="360" onInput={handleHueChange} />
-      <p>S: </p>
-      <p>L: </p>
+      <p>S: {saturation}</p>
+      <input
+        type="range"
+        value={saturation}
+        max="100"
+        onInput={handleSaturationChange}
+      />
+      <p>L: {lighting} </p>
+      <input
+        type="range"
+        value={lighting}
+        max="50"
+        onInput={handleLightingChange}
+      />
     </>
   )
 }
